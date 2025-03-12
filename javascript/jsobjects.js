ddb54edd6c26811bcafbc2a6cea2646dc39b377a
KinyanjuiC <collinskimani6@gmail.com>
2025-03-12 14:10:47 +0300

    updated jsobjects.html

--- javascript/jsobjects.js
+++
@@ -1,9 +0,0 @@
-let car ={
-    make: "Toyota",
-    model: "Camry",
-    color: "white",
-    speed: 200,
-};
-
-console.log(car); // output the entire object
-console.log(car.make); // accessing property of the object
