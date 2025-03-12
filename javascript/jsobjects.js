c7002b6fbc55e15379bb6b0552a32dbae861574f
KinyanjuiC <collinskimani6@gmail.com>
2025-03-12 13:52:52 +0300

    Added jsobjects1

---
+++ javascript/jsobjects.js
@@ -0,0 +1,9 @@
+let car ={
+    make: "Toyota",
+    model: "Camry",
+    color: "white",
+    speed: 200,
+};
+
+console.log(car); // output the entire object
+console.log(car.make); // accessing property of the object
